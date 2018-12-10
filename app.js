d81e5311b78b0e72738aa60da03052c88fa95aef
abrar000 <abrar@uconnect.pk>
2018-12-10 15:25:06 +0500

    adding more type conversions

--- app.js
+++ app.js
@@ -1,17 +1,154 @@
+console.log("" + 1 + 0); // 10 
+console.log("abrar" + 1 + 0); // abrar10
+console.log( 1 + 0); // 1
+console.log("" - 1 - 0); // -1
+console.log("" + 1 - 0); // 1
+console.log("" - 1 - 1); // -2
+console.log("" - 1 - "0"); // -1
+console.log(- 1 - "0"); // -1
+console.log("abrar" - 1 - "0"); // nan
+console.log("" - 0 - 0); // 0
+console.log("" + ""); // empty space ---------------
+console.log("" - "" ); // 0
+console.log("" - 1 + 3); // 2
+console.log(true + false); // 1
+console.log(1+0); // 1
+console.log(0-1); // -1
+console.log("6"/"3"); // 2
+console.log("6" + "3"); // 63
+console.log(6 / 3); // 2
+console.log("6" * "3"); // 18
+console.log("6" - "3"); // 3
+console.log( "6" == "3"); // false
+console.log("6" === "3"); // false
+console.log("2" * "3"); // 6
+console.log("4" + 5 + "px"); // 45px
+console.log("4" + "5" + "px"); // 45px
+console.log(true + false); // 1
+console.log(1+0); // 1
+console.log(0-1); //-1
+console.log("6" + "3"); // 63
+console.log(6 / 3); // 2
+console.log("6" * "3");  // 18
+console.log("6" - "3"); //3
+console.log( "6" == "3"); // false
+console.log("6" === "3"); // false
+console.log("2" * "3"); // 6
+console.log("4" + 5 + "px"); // 45px------------------
+console.log("4" + "5" + "px"); // 45px-----------------
+console.log(4+5+"px"); // 9px----------------------------
+console.log("px" + 4+5); //px9 ----------------
+console.log("px" - 4+5); // nan -------------------
+console.log(true + false); // 1
+console.log(1+0); //1
+console.log(0-1);//-1
+console.log("6" / "3"); //2
+console.log("6" + "3"); //63
+console.log(6 / 3); //2
+console.log("6" * "3");//18
+console.log("6" - "3"); //3
+console.log( "6" == "3"); //false
+console.log("6" === "3"); //false
+console.log("2" * "3"); // 6
+console.log("4" + 5 + "px"); // 45px
+console.log("4" + "5" + "px"); // 45px
+console.log("4" + "5" - "px"); //nan
+console.log("$" + 4 + 5); // $45 ------------------
+console.log( 4 + 5 + "$"); //9$ ------------------
+console.log("$" - "4" - "5"); // nan
+console.log("" - "4" - "5"); // -9
+console.log("$\n" - 5); // nan
+console.log("abrar" + 5); // abrar5
+console.log("4" - 2); // 2
+console.log("4px" + 2); // 4px2
+console.log("4px" - 2); // nan
+console.log(7 / 0); // infinity ---------------
+console.log(7 / 2); // 3.5
+console.log(7 + 0); // 7
+console.log(0 / 0); // nan  ----------------------
+console.log(0 + 0); // 0
+console.log(0 - 0); // 0
+console.log(0 / 7);  //  0
+console.log(7 * 0); //0
+console.log(0 * 7); // 0
+console.log("-9\n" + "5"); //-9 and the 5 will display in next line
+console.log("-9    " + "5"); // -9     5
+console.log("-9\n" - "-5\n"); // -4
+console.log("-9\n" - "5\n"); // -14
+console.log(null + 1); // 1
+console.log(null + null); // 0
+console.log(undefined + undefined); // nan
+console.log(undefined + 1); //nan
+console.log("true" + "false"); // truefalse
+console.log(true + false); // 1
+console.log(true == false); // false
+console.log(true + true); // 2
+console.log(true - true); // 0
+console.log(true / true); //1
+console.log(true * true); // 1
+console.log(undefined + 1); // nan
+console.log("true" + "false"); // truefalse
+console.log(true + false); // 1
+console.log(true == false); // false
+console.log(true + true); // 2
+console.log(true - true); // 0
+console.log(true / true); // 1
+console.log(true * true); // 1
+console.log(true == true); //true
+console.log(true + true); // 2
+console.log(false + false); // 0
+console.log(true + false); // 1
+console.log(undefined == null); // true ---------------------------????
+console.log(undefined + null); // nan
+console.log(undefined - null); // nan
+console.log(null + null);  // 0
+console.log(null - null); // 0
+console.log(null * null); // 0
+console.log(null == null); //true
+console.log(undefined + undefined); //nan
+console.log(null + 2); // 2
+console.log(null - 2); // -2
+console.log(null/null); //nan-----------------
+console.log("" / null); //
+console.log("")
+// if ('0') {
+//   alert("hello");
+// }
+
+ 
+let a = 2 ,b = 10;
+
+if (a+b < 4){
+  alert ("hello")
+}
+else{
+  alert ("hiiiii")
+}
 
 
-console.log("" + 1 + 0);
-console.log("" - 1 + 0);
-console.log(true + false);
-console.log(6 / 3);
-console.log("2" * "3");
-console.log(4 + 5 + "px");
-console.log("$" + 4 + 5);
-console.log("4" - 2);
-console.log("4px" - 2);
-console.log(7 / 0);
-console.log("-9\n" + 5);
-console.log("-9\n" - 5);
-console.log(null + 1);
-console.log(undefined + 1);
+
+// in division if zero c0mes after than it becomes infinity
+// if the zero divide with zero it becomes not a number 
+// null is equal to zero 
+// null is equal to undifined  but undefined acts differentlly.....??????
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
